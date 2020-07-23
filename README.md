4bc4bc24bba1431be8a051c6cee9b149fbc52e06
Genna Mergola <gennamergola@gmail.com>
2020-07-23 11:23:46 -0700

    changed settings to work in browsers other than chrome

--- README.md
+++
@@ -1,5 +0,0 @@
-## Getting started with Microblog's frontend
-
-1. Clone this repo and `cd` into `microblog-frontend`
-2. `npm install`
-3. `npm start`
--- src/index.js
+++ src/index.js
@@ -11,13 +11,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__
+//     && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-    && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+    window.navigator.userAgent.includes('Chrome') ?
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
+  ),
 );
 
 
